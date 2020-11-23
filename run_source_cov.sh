71f5783b59cc1834789ad4eb2b6aadab3695e4b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-23 21:30:09 +0100

    Remove some unneeded commands

--- run_source_cov.sh
+++ run_source_cov.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-rm -rf ./target *.prof* *.info
+rm -rf ./target *.prof*
 
-export RUSTFLAGS="-Zinstrument-coverage -Cprefer-dynamic"
-export LD_LIBRARY_PATH="/home/marco/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/"
+export RUSTFLAGS="-Zinstrument-coverage"
 
 cargo build
 ./target/debug/rust-code-coverage-sample
-# llvm-profdata merge -sparse default.profraw -o default.profdata
-# llvm-cov export --Xdemangler=rustfilt target/debug/rust-code-coverage-sample --instr-profile=default.profdata --format lcov > output.info
-grcov . --binary-path ./target/debug/rust-code-coverage-sample -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov . --binary-path ./target/debug/rust-code-coverage-sample -s . -t html --branch --ignore-not-existing -o ./target/debug/coverage/
